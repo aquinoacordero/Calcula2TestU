b901683189fbbdf3289b61b2fba8b88a75241e3b
aferreiradominguez <aferreiradominguez@atanasoft05.danielcastelao.org>
2015-04-16 10:05:23 +0200

    falta hacer que funcione

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -4,26 +4,22 @@ import java.util.Scanner;
 
 public class Calculadora {
 
-    public static void main(String[] args) {
-        Modelo mode = new Modelo();
-        Calculadora calc = new Calculadora();
-        Vista.imprimir(calc.realizaOperacion(mode.getNum1(), mode.getNum2(), mode.getOperacion()));
-    }
-
     public String realizaOperacion(float n1, float n2, String op) {
         Modelo mode = new Modelo();
         System.out.println("introduce la operación que deseas\nsumar\nrestar\nmultiplicar\ndividir\n");
         Scanner dato = new Scanner(System.in);
-        op = dato.nextLine();
+        mode.setOperacion(dato.nextLine()); 
+        op=mode.getOperacion();
         System.out.println("introduce el primer operando");
         Scanner num = new Scanner(System.in);
         mode.setNum1(num.nextFloat());
         System.out.println("introduce el segundo operando");
         Scanner num1 = new Scanner(System.in);
-        mode.setNum2(num.nextFloat());
+        mode.setNum2(num1.nextFloat());
 
         if ("sumar".equals(op)) {
-            return ("suma=" + (n1 + n2));
+            String suma = String.valueOf((n1 + n2));
+            return suma;
         }
         if ("restar".equals(op)) {
             return ("resta=" + (n1 - n2));
@@ -37,6 +33,12 @@ public class Calculadora {
             return "inroduce operación valida";
         }
 
+    }
+
+    public static void main(String[] args) {
+        Modelo mode = new Modelo();
+        Calculadora calc = new Calculadora();
+        Vista.imprimir(calc.realizaOperacion(mode.getNum1(), mode.getNum2(), mode.getOperacion()));
     }
 
 }
