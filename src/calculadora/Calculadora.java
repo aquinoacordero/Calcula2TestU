792333b4c9a725dc5a91ffdb5376c5801b49a3e7
aferreiradominguez <aferreiradominguez@atanasoft05.danielcastelao.org>
2015-04-16 10:27:19 +0200

    ended

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -4,18 +4,11 @@ import java.util.Scanner;
 
 public class Calculadora {
 
+    static Modelo mode = new Modelo();
+
     public String realizaOperacion(float n1, float n2, String op) {
-        Modelo mode = new Modelo();
-        System.out.println("introduce la operación que deseas\nsumar\nrestar\nmultiplicar\ndividir\n");
-        Scanner dato = new Scanner(System.in);
-        mode.setOperacion(dato.nextLine()); 
-        op=mode.getOperacion();
-        System.out.println("introduce el primer operando");
-        Scanner num = new Scanner(System.in);
-        mode.setNum1(num.nextFloat());
-        System.out.println("introduce el segundo operando");
-        Scanner num1 = new Scanner(System.in);
-        mode.setNum2(num1.nextFloat());
+
+        op = mode.getOperacion();
 
         if ("sumar".equals(op)) {
             String suma = String.valueOf((n1 + n2));
@@ -30,14 +23,23 @@ public class Calculadora {
         if ("dividir".equals(op)) {
             return ("division=" + (n1 / n2));
         } else {
-            return "inroduce operación valida";
+            return "introduce operación valida";
         }
 
     }
 
     public static void main(String[] args) {
-        Modelo mode = new Modelo();
+        // Modelo mode = new Modelo();
         Calculadora calc = new Calculadora();
+        System.out.println("introduce la operación que deseas\nsumar\nrestar\nmultiplicar\ndividir\n");
+        Scanner dato = new Scanner(System.in);
+        mode.setOperacion(dato.nextLine());
+        System.out.println("introduce el primer operando");
+        Scanner num = new Scanner(System.in);
+        mode.setNum1(num.nextFloat());
+        System.out.println("introduce el segundo operando");
+        Scanner num1 = new Scanner(System.in);
+        mode.setNum2(num1.nextFloat());
         Vista.imprimir(calc.realizaOperacion(mode.getNum1(), mode.getNum2(), mode.getOperacion()));
     }
 
