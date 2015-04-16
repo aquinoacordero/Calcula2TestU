af94381f202817c344a1eb1dcbd1a403b4d28987
aferreiradominguez <aferreiradominguez@atanasoft05.danielcastelao.org>
2015-04-16 09:56:05 +0200

    debug state

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -6,12 +6,22 @@ public class Calculadora {
 
     public static void main(String[] args) {
         Modelo mode = new Modelo();
-        Calculadora calc=new Calculadora();
+        Calculadora calc = new Calculadora();
         Vista.imprimir(calc.realizaOperacion(mode.getNum1(), mode.getNum2(), mode.getOperacion()));
     }
 
     public String realizaOperacion(float n1, float n2, String op) {
-        Scanner dato=new Scanner(System.in);
+        Modelo mode = new Modelo();
+        System.out.println("introduce la operación que deseas\nsumar\nrestar\nmultiplicar\ndividir\n");
+        Scanner dato = new Scanner(System.in);
+        op = dato.nextLine();
+        System.out.println("introduce el primer operando");
+        Scanner num = new Scanner(System.in);
+        mode.setNum1(num.nextFloat());
+        System.out.println("introduce el segundo operando");
+        Scanner num1 = new Scanner(System.in);
+        mode.setNum2(num.nextFloat());
+
         if ("sumar".equals(op)) {
             return ("suma=" + (n1 + n2));
         }
